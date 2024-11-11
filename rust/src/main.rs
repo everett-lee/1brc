7116bd273099dce18e248f3bb1f22f2c10a8aea4
lee <lee.m.everett@gmail.com>
2024-11-11 22:21:43 +0000

    Multithreaded

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,7 @@
 use std::collections::HashMap;
-use std::fmt;
+use std::{fmt, thread};
 use std::fmt::{Display, Formatter};
 use std::io::prelude::*;
-use std::io::BufReader;
 use std::fs::File;
 use std::time::Instant;
 use memmap2::Mmap;
@@ -59,14 +58,17 @@ impl Display for Collector {
     }
 }
 
-fn process_line(ln: &str, cities: &mut HashMap<String, Collector>) {
+fn process_line(ln: &str) {
+    let mut cities: HashMap<String, Collector> = HashMap::new();
+
     let vals: Vec<String> = ln.split(';')
         .map(|s| String::from(s)).collect();
     let (city, reading) = (&vals[0], &vals[1]);
 
     // Convert float string to integer for faster arithmetic
     // E.g. 42.1 becomes 421
-    let temp = reading.parse::<f32>().unwrap();
+    // TODO check this is actually helpful
+    let temp = reading.parse::<f32>().expect("Error parsing temperature");
     let temp_normalised = (temp * 10.0) as i32;
 
     match cities.contains_key(city.as_str()) {
@@ -83,41 +85,80 @@ fn process_line(ln: &str, cities: &mut HashMap<String, Collector>) {
     };
 }
 
-fn main() {
-    let start = Instant::now();
+fn get_lines_range(mmap: &Mmap, start_line: usize, num_lines: usize) -> Option<(usize, usize)> {
+    let mut line_count = 0;
+    let mut start_index = 0;
+    let mut end_index = 0;
 
-    let mut cities: HashMap<String, Collector> = HashMap::new();
+    for (i, &byte) in mmap.iter().enumerate() {
+        if byte == b'\n' {
+            line_count += 1;
+
+            if line_count == start_line {
+                start_index = i + 1; // Start of the first line
+            }
+
+            // If we've reached the n-th line
+            if line_count == start_line + num_lines {
+                end_index = i; // End of the nth line
+                break;
+            }
+        }
+    }
+
+    // TODO fix this to return exact number
+    // Probably just return end index?
+    if line_count >= start_line + num_lines {
+        Some((start_index, end_index))
+    } else {
+        None // If there are not enough lines
+    }
+}
+
+fn main() {
+    let start_time = Instant::now();
 
     let fp = "/home/lee/Projects/1brc-data/1brc/measurements.txt";
     let file = File::open(fp).unwrap();
     let mmap = unsafe { Mmap::map(&file).unwrap() };
-    let content = std::str::from_utf8(&mmap).unwrap();
-    let lines: Vec<&str> = content.lines().collect();
 
-    let mut count = 0;
-    for line in lines {
-        count += 1;
-        process_line(line, &mut cities);
+    let mut start_line = 0;
+    let num_lines = 100_000_000;
 
-        if count % 1000000 == 0 {
-            println!("On line {}", count);
-        }
+    let mut handles = vec![];
+    while let Some((start_index, end_index)) = get_lines_range(&mmap, start_line, num_lines) {
+        let data_chunk = String::from_utf8_lossy(&mmap[start_index..end_index]).to_string();
 
-        // if count > 10_000_000 {
-        //     break
-        // }
+        let handle = thread::spawn(move || {
+            println!("Readling lines from {}", start_line);
+            data_chunk.split("\n").for_each(
+                |l| process_line(l)
+            );
+            println!("Finished reading lines from {}", start_line);
+        });
+        handles.push(handle);
+
+        start_line += num_lines;
     }
-    println!("DONEZO");
-    for (key, value) in &cities {
-        println!("City: {}, res: {}", key, value);
+    for handle in handles {
+        handle.join().unwrap();
     }
 
-    let duration = start.elapsed();
+    // for (key, value) in &cities {
+    //     println!("City: {}, res: {}", key, value);
+    // }
+
+    let duration = start_time.elapsed();
     println!("Elapsed time: {} ms", duration.as_millis());
 }
 
 // TODO
 // threading
+// create collector struct in thread
+// combine collectors at end
+
 // mmap
 // better buffered reading?
+
+// save as json
 
