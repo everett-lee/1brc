af61670d41fecb32ed87c5988d69282ca330ff0a
lee <lee.m.everett@gmail.com>
2024-11-11 23:49:05 +0000

    Char skipping version

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,11 +1,13 @@
 use std::collections::HashMap;
 use std::{fmt, thread};
+use std::cmp::{max, min};
 use std::fmt::{Display, Formatter};
 use std::io::prelude::*;
 use std::fs::File;
 use std::time::Instant;
 use memmap2::Mmap;
 
+#[derive(Clone)]
 struct Collector {
     min: i32,
     max: i32,
@@ -16,6 +18,15 @@ struct Collector {
 impl Collector {
     pub fn new(starting_val: i32) -> Collector {
         Collector {min: starting_val, max: starting_val, count: 1, sum: starting_val}
+    }
+
+    pub fn add(&self, other: Collector) -> Collector {
+        Collector {
+            min: min(self.min, other.min),
+            max: max(self.max, other.max),
+            count: self.count + other.count,
+            sum: self.sum + other.sum
+        }
     }
 
     pub fn set_min(&mut self, new_val: i32) {
@@ -49,20 +60,18 @@ impl Collector {
 impl Display for Collector {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         write!(
-            f, "{} {} {} {}",
-            self.count,
-            self.sum,
-            self.min,
-            self.max
+            f, "{} {} {}",
+            (self.sum as f64 / 10.0) / self.count as f64,
+            self.min as f64 / 10.0,
+            self.max as f64 / 10.0
         )
     }
 }
 
-fn process_line(ln: &str) {
-    let mut cities: HashMap<String, Collector> = HashMap::new();
-
+fn process_line(ln: &str, cities: &mut HashMap<String, Collector>) {
     let vals: Vec<String> = ln.split(';')
         .map(|s| String::from(s)).collect();
+
     let (city, reading) = (&vals[0], &vals[1]);
 
     // Convert float string to integer for faster arithmetic
@@ -85,70 +94,89 @@ fn process_line(ln: &str) {
     };
 }
 
-fn get_lines_range(mmap: &Mmap, start_line: usize, num_lines: usize) -> Option<(usize, usize)> {
-    let mut line_count = 0;
-    let mut start_index = 0;
-    let mut end_index = 0;
-
-    for (i, &byte) in mmap.iter().enumerate() {
-        if byte == b'\n' {
-            line_count += 1;
-
-            if line_count == start_line {
-                start_index = i + 1; // Start of the first line
-            }
-
-            // If we've reached the n-th line
-            if line_count == start_line + num_lines {
-                end_index = i; // End of the nth line
-                break;
-            }
-        }
+fn get_next_n_chars(mmap: &Mmap, start: usize, n_chars: usize) -> (usize, usize) {
+    let mut end = start + n_chars;
+    if end >= mmap.len() {
+        return (start, mmap.len())
     }
 
-    // TODO fix this to return exact number
-    // Probably just return end index?
-    if line_count >= start_line + num_lines {
-        Some((start_index, end_index))
-    } else {
-        None // If there are not enough lines
+    let char_at_end = mmap.get(end);
+    // Take from start .. end of memory
+    if char_at_end.is_none() {
+        return (start, mmap.len());
     }
+    if *char_at_end.unwrap() == b'\n' {
+        return (start, end)
+    }
+
+    let mut current = char_at_end.unwrap();
+    // If not at newline keep iterating until we fine one
+    while *current != b'\n' {
+        end += 1;
+        // Can unwrap as has to end with newline
+        current = mmap.get(end).unwrap()
+    }
+    (start, end)
 }
 
 fn main() {
     let start_time = Instant::now();
-
     let fp = "/home/lee/Projects/1brc-data/1brc/measurements.txt";
     let file = File::open(fp).unwrap();
     let mmap = unsafe { Mmap::map(&file).unwrap() };
 
-    let mut start_line = 0;
-    let num_lines = 100_000_000;
-
+    let n_chars = 500_000_000;
     let mut handles = vec![];
-    while let Some((start_index, end_index)) = get_lines_range(&mmap, start_line, num_lines) {
-        let data_chunk = String::from_utf8_lossy(&mmap[start_index..end_index]).to_string();
+    let (mut start_char_index, mut end_char_index) = get_next_n_chars(&mmap, 0, n_chars);
+
+    let mut count = 0;
+    while end_char_index < mmap.len() {
+        let data_chunk = String::from_utf8_lossy(&mmap[start_char_index..end_char_index]).to_string();
+
+        // count += 1;
+        // if count > 5 {
+        //     break
+        // }
 
         let handle = thread::spawn(move || {
-            println!("Readling lines from {}", start_line);
+            println!("Reading chars between {} and {}", start_char_index, end_char_index);
+            let mut cities: HashMap<String, Collector> = HashMap::new();
+
             data_chunk.split("\n").for_each(
-                |l| process_line(l)
+                |l| {
+                    if l.len() > 1 {
+                        process_line(l, &mut cities);
+                    }
+                }
             );
-            println!("Finished reading lines from {}", start_line);
+            println!("Finished chars lines to {}", end_char_index);
+            cities
         });
         handles.push(handle);
 
-        start_line += num_lines;
-    }
-    for handle in handles {
-        handle.join().unwrap();
+        (start_char_index, end_char_index) = get_next_n_chars(&mmap, end_char_index, n_chars);
     }
 
-    // for (key, value) in &cities {
-    //     println!("City: {}, res: {}", key, value);
-    // }
+
+    let mut final_cities: HashMap<String, Collector> = HashMap::new();
+    for handle in handles {
+        let cities = handle.join().unwrap();
+        cities.iter().for_each(|(city, collector)| {
+            if final_cities.contains_key(city) {
+                let existing = final_cities.get(city).unwrap();
+                final_cities.insert(String::from(city), existing.add(collector.clone()));
+            } else {
+                final_cities.insert(String::from(city), collector.clone());
+            }
+        })
+    }
 
     let duration = start_time.elapsed();
+
+    final_cities.iter().for_each(|(city, collector)| {
+        println!("{} {}", city, collector);
+    });
+
     println!("Elapsed time: {} ms", duration.as_millis());
 }
 
